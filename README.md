13bfe34d73e115eb047a8c70ba61651368c04068
ethannjw <ethannjw@hotmail.com>
2020-09-25 11:59:24 +0800

    completed documentation

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# name-sorter
+# name-sorter program
+The name sorter program reads in a file with names separated by return characters (\n) and provides cleaning and sorting methods.
+Finally, the program allows for the user to save the sorted names to another file
+
+## Initialise:
+First import the module:
+> import name_sorter
+
+Pass in the full path of file to the class by calling:
+> ns = NameSorter(file_path)
+
+That's it! Now you can maniplate your list of names
+
+## Clean names
+Call the clean() method by
+> ns.clean()
+
+## Sort names
+Sort the names by using the sort() method
+The program uses quicksort algorithm that allow for O(n log n) time complexity
+> ns.sort()
+
+## Print names to screen
+To print the names to screen simply:
+> print(ns)
+
+## Save the names
+To save the names to a specified file
+> ns.save(file_path)
+
+Sample text with fictious names are provided
--- src/name_sorter.py
+++ src/name_sorter.py
@@ -1,23 +1,46 @@
 """
 File:       name_sorter.py
 Purpose:    gets a list of names from a file and sorts the names, before
-            printing the sorted names to screen
+            printing the sorted names to screen, then outputs the sorted names
+            to another text file
+Author:     Ethan Ng
+Source:     https://github.com/ethannjw/name-sorter.git
 """
 
 import csv
 import os
-from os import path
 
 class NameSorter:
-    def __init__(self, file_path):
-        self.list = self.openfile(file_path)
 
-    @staticmethod
-    def quicksort(x):
+    def __init__(self, file_path):
+        """initialises the class by reading the file and storing into an attribute"""
+        self._openfile(file_path)
+
+    def clean(self):
         """
+        Public method that cleans the list of names by removing trailing spaces
+        """
+        if len(self.list_of_names) > 0:
+            for name in self.list_of_names:
+                name[0] = name[0].strip()
+
+    def __repr__(self):
+        """
+        To string method to print out the list of names present in the class
+        @return string of list of names separated by return char (\n)
+        """
+        str = ""
+        for name in self.list_of_names:
+            str = str + f"{name[0]}\n"
+        return str
+
+    def quicksort(self, x):
+        """
+        Static Method that sorts a list using the quick algorithm
         Quicksort is a divide and conquer strategy algorithm that first selects a pivot,
         then move the smaller elments to the left and the larger ones to the right,
-        repeating this till the array is sorted
+        repeating this till the array is sorted.
+
         @param array to be sorted
         @return sorted array
         """
@@ -31,50 +54,54 @@ class NameSorter:
                     x[j+1],x[i+1] = x[i+1], x[j+1]
                     i += 1
             x[0],x[i] = x[i],x[0]
-            first_part = quicksort(x[:i])
-            second_part = quicksort(x[i+1:])
+            first_part = self.quicksort(x[:i])
+            second_part = self.quicksort(x[i+1:])
             first_part.append(x[i])
             return first_part + second_part
 
-    def clean(self):
-        """
-        Cleans the list of names passed into the function and returns a generator
-        @param list
-        @return generator of cleaned names
-        """
-        if len(self.list) > 0:
-            for name in self.list:
-                name[0] = name[0].strip()
-                yield name[0]
+    def sort(self):
+        """Public Method that sorts the list_of_names in class"""
+        self.list_of_names = self.quicksort(self.list_of_names)
 
-    @staticmethod
-    def openfile(file_name):
+    def _openfile(self, file_name):
         """
-        Open the file with read permission using a context manager
+        Private method that opens and reads the file and saves the file in the
+        class
         @param file name of file
         @return list of entries in file
         """
         with open(file_name, "r") as file:
             reader = csv.reader(file)
-            data = list(reader)
-        return data
+            self.list_of_names = list(reader)
 
+    def save(self, file_name):
+        """
+        Saves the file with a specified file path
+        @param file name of file
+        @return list of entries in file
+        """
+        with open(file_name, "w") as file:
+            for name in self.list_of_names:
+                file.write(f"{name[0]}\n")
 
 def main():
     """
     Main driver code
     """
     current_folder = os.getcwd()    # get the path of current folder
-    parent_folder = path.dirname(current_folder)    # get the path of parent folder
-    file_path = path.join(parent_folder, "unsorted-names-list.txt") # get the file path
+    parent_folder = os.path.dirname(current_folder)    # get the path of parent folder
+    file_path = os.path.join(parent_folder, "unsorted-names-list.txt") # get the file path
     # open the file and read data
     ns = NameSorter(file_path)  # instance of name sorter
+    print(f"After import:\n{ns}\n")
     # clean the data
-    cleaned_data = list(ns.clean())
+    ns.clean()
+    print(f"After cleaning:\n{ns}\n")
     # Sort the data
-    sorted_data = ns.quicksort(cleaned_data)
-    # print the data using a list comprehension
-    [print(name) for name in sorted_data]
+    ns.sort()
+    print(f"After sorting:\n{ns}\n")
+    # output to file
+    ns.save(os.path.join(parent_folder, "sorted-names-list.txt"))
 
 if __name__ == "__main__":
     main()
